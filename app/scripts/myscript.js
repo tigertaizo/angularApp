2cf5de1283d2da7a055e9424e2aa85bf668ff105
tiger taizo <no14141@gmail.com>
2015-05-19 23:22:45 +0900

    Initial commit

---
+++ app/scripts/myscript.js
@@ -0,0 +1,20 @@
+var app = angular.module('myApp', []);
+
+
+app.controller('mainCtrl', function($scope) {
+  //$scope.data = {message: "Hello"};
+  $scope.users = [
+    {"name":"taguchi","score":78.2},
+    {"name":"nakata","score":83.8},
+    {"name":"ikeda","score":55.4},
+    {"name":"yamada","score":23.2},
+    {"name":"takeshi","score":56.4},
+    {"name":"ishii","score":100},
+    {"name":"takeda","score":78.2},
+  ];
+  $scope.today =new Date();
+
+
+
+
+});
